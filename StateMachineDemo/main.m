4824a1cd08f53ef6bac335659f292951b9649255
Vladislav <rct.stankevivs@gmail.com>
2021-02-14 23:36:13 +0300

    Initial Commit

---
+++ StateMachineDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  StateMachineDemo
+//
+//  Created by Uladzislau Stankevich on 14.02.21.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
